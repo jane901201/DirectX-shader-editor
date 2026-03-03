d9435e2ab87e21c48eb216853b835c385af71bc7
MSI\HORUSHIUAN <jane36742715@gmail.com>
2026-03-03 23:49:32 +0800

    新增 bootstrap.bat 來設定環境

---
+++ bootstrap.bat
@@ -0,0 +1,20 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+set ROOT=%~dp0..
+pushd "%ROOT%"
+
+echo [1/2] Checking vcpkg...
+if not exist "%ROOT%\vcpkg\bootstrap-vcpkg.bat" (
+  echo vcpkg not found. Cloning...
+  git clone https://github.com/microsoft/vcpkg "%ROOT%\vcpkg"
+  if errorlevel 1 exit /b 1
+)
+
+echo [2/2] Bootstrapping vcpkg...
+call "%ROOT%\vcpkg\bootstrap-vcpkg.bat"
+if errorlevel 1 exit /b 1
+
+echo vcpkg setup complete.
+popd
+endlocal
